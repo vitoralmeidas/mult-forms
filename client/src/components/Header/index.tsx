423395bb232ecffa25b33af3b76880bc23c6c749
vitoralmeidas <vitor.oliveira1904@gmail.com>
2022-07-12 18:14:43 -0300

    recruiter page

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -3,8 +3,11 @@ import * as C from "./styles";
 export const Header = () => {
   return (
     <C.Container>
+      <div>
       <h1>Cadastro de Desenvolvedor</h1>
       <p>Faça seu cadastro na lista e receba um emprego instantaneamente.</p>
+      </div>
+      <button>Recrutador</button>
     </C.Container>
   );
 };
--- client/src/components/Header/styles.ts
+++ client/src/components/Header/styles.ts
@@ -3,6 +3,9 @@ import styled from "styled-components";
 export const Container = styled.div`
   padding: 25px 0;
   border-bottom: 1px solid #16195c;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
   h1 {
     margin: 0;
@@ -13,5 +16,11 @@ export const Container = styled.div`
   p {
     font-size: 20px;
     color: #b8b8d4;
+  }
+
+  button {
+    font-size: 15px;
+    padding: 10px;
+    border-radius: 10%;
   }
 `;
