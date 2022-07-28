d99f550541adbac1b88e782f88d8a1dc00b05d20
vitoralmeidas <vitor.oliveira1904@gmail.com>
2022-07-28 19:46:22 -0300

    Login page -> basic styles + basic logic

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import  * as C from "./styles";
 
+
 export const Login = () => {
+    const [recruiterPassword, setRecruiterPassword] = useState('')
+    const [recruiterEmail, setRecruiterEmail] = useState('')
+
+    const passwordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setRecruiterPassword(e.target.value)
+    }
+
+    const emailHandler = (e: React.ChangeEvent<HTMLInputElement>) =>{
+        setRecruiterEmail(e.target.value)
+    }
+
+
     return (
-        <div>
-        <h1>LOGIN</h1>
-        <Link to="/recruiter"><button>Recrutador</button></Link>
-        </div>
+        <C.Container>
+            <div>
+                <div className="content">
+                    <h1>RECRUTADOR</h1>
+                    <form>
+                        <input placeholder="Email" value={recruiterPassword} onChange={passwordHandler}  />
+                        <input placeholder="Password" value={recruiterEmail} onChange={emailHandler} />
+                        <button>Login</button>
+                    </form>
+                    <button>Register</button>
+                    <Link to="/recruiter"><button>Recrutador</button></Link>
+                </div>
+        
+            </div>            
+        </C.Container>
     );
 }
